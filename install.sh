0d236f7b958e30e8db04ec4f7afd4d09fae58e0a
gilsondev <me@gilsondev.in>
2015-05-13 09:44:32 -0300

    Prepare configurations

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s ~/.dotenv/zshrc ~/.zshrc
+ln -s ~/.dotenv/autoenv ~/.autoenv
+ln -s ~/.dotenv/vimrc ~/.vimrc
+ln -s ~/.dotenv/fonts ~/.fonts
+ln -s ~/.dotenv/gitconfig ~/.gitconfig
