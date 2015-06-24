6ac46b44fb5a190406996186ce00af0bcc4a8ec6
gilsondev <me@gilsondev.in>
2015-06-24 00:14:04 -0300

    Update install and zshrc

--- install.sh
+++ install.sh
@@ -5,8 +5,35 @@ git submodule init
 git submodule update
 
 # Create symbol links
-ln -s ~/.dotenv/zshrc ~/.zshrc
-ln -s ~/.dotenv/autoenv ~/.autoenv
-ln -s ~/.dotenv/vimrc ~/.vimrc
-ln -s ~/.dotenv/fonts ~/.fonts
-ln -s ~/.dotenv/gitconfig ~/.gitconfig
+if [ -L "~/.zshrc" ]; then
+	rm -rf ~/.zshrc
+	ln -s ~/.dotenv/zshrc ~/.zshrc
+fi
+
+if [ -L "~/.autoenv" ]; then
+	rm -rf ~/.autoenv
+	ln -s ~/.dotenv/autoenv ~/.autoenv
+fi
+
+if [ -L "~/.vimrc" ]; then
+	rm -rf ~/.vimrc
+	ln -s ~/.dotenv/vimrc ~/.vimrc
+fi
+
+if [ -L "~/.fonts" ]; then
+	rm -rf ~/.gitconfig
+	ln -s ~/.dotenv/fonts ~/.fonts
+fi
+
+if [ -L "~/.gitconfig" ]; then
+	rm -rf ~/.gitconfig
+	ln -s ~/.dotenv/gitconfig ~/.gitconfig
+fi
+
+
+# Install NVM
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
+
+# Install GVM
+sudo apt-get install curl -y
+curl -s get.gvmtool.net | bash
--- zshrc
+++ zshrc
@@ -56,9 +56,9 @@ export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/g
 source ~/.autoenv/activate.sh
 
 # Pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+# export PYENV_ROOT="$HOME/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init -)"
 
 # Virtualenvwrapper
 export WORKON_HOME=$HOME/Developer/.virtualenvs
